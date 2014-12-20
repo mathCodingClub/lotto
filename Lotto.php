95d2304386a2e19b3e9ca3d9e89b16587ed885ab
Antti Stenvall <stenvala@Anttis-MacBook-Pro.local>
2014-12-20 18:18:46 +0200

    Just something, don't remember anymore

--- Lotto.php
+++ Lotto.php
@@ -5,6 +5,7 @@ namespace Lotto;
 class Lotto {
 
   private $db = null;
+
   const databaseName = 'mcc_lotto';
 
   public function __construct() {
@@ -12,7 +13,7 @@ class Lotto {
     $this->db->setShowError(true);
   }
 
-  public function get(){
+  public function get() {
 
     $query = $this->db->prepare("SELECT * FROM rivi WHERE pvm IN (SELECT max(pvm) FROM rivi)");
 
@@ -24,28 +25,23 @@ class Lotto {
     $round = 0;
     $year = 0;
 
-    foreach($query as $row)
-    {
+    foreach ($query as $row) {
 
       $basic = $row['varsiNroa'];
       $extra = $row['extraNroa'];
       $round = $row['kierros'];
       $year = $row['vuosi'];
 
-      for($i = 1; $i <= $basic; $i++)
-      {
+      for ($i = 1; $i <= $basic; $i++) {
         $nro = $row['num' . $i];
-        if($nro != 0)
-        {
+        if ($nro != 0) {
           $basic_numbers[] = $nro;
         }
       }
 
-      for($i = $basic+1; $i <= $basic+$extra; $i++)
-      {
+      for ($i = $basic + 1; $i <= $basic + $extra; $i++) {
         $nro = $row['num' . $i];
-        if($nro != 0)
-        {
+        if ($nro != 0) {
           $extra_numbers[] = $nro;
         }
       }
@@ -55,15 +51,13 @@ class Lotto {
     }
 
     return "Correct row for round \"$round / $year\" is: [ " . join(" ", $basic_numbers) . " + "
-           . join(" ", $extra_numbers) . " ], cast on " . $date;
+        . join(" ", $extra_numbers) . " ], cast on " . $date;
   }
 
-  public function getLatestMatch($nro1,$nro2,$nro3,$nro4,$nro5,$nro6,$nro7){
-return "not implemented yet, thanks for $nro1,$nro2,$nro3,$nro4,$nro5,$nro6,$nro7";
+  public function getLatestMatch($nro1, $nro2, $nro3, $nro4, $nro5, $nro6, $nro7) {
+    return "not implemented yet, thanks for $nro1,$nro2,$nro3,$nro4,$nro5,$nro6,$nro7";
   }
 
 }
-
-
 
 ?>
